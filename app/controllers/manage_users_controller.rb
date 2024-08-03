60899bd1a0bd173a3012389d420c7ae12f5d820c
Rich Lobo <lobo.r0018@gmail.com>
2024-08-03 17:28:00 +0800

    Adds status field to user

--- app/controllers/manage_users_controller.rb
+++ app/controllers/manage_users_controller.rb
@@ -8,4 +8,38 @@ class ManageUsersController < ApplicationController
     def show
         @user = User.find(params[:id])
     end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        params.permit!
+        @user = User.create(email: params['user']['email'], password: params['user']['password'], role: params['user']['role'], name: params['user']['name'], surname: params['user']['surname'], status: params['user']['status']);
+        if @user.save
+            redirect_to manage_user_path(@user)
+        else
+            render :new
+        end
+    end
+
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+        @user.update(email: params['user']['email'], password: params['user']['password'], role: params['user']['role'], name: params['user']['name'], surname: params['user']['surname'], status: params['user']['status']);
+        if @user.save
+            redirect_to manage_user_path(@user)
+        else
+            render :new
+        end
+    end
+
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+        redirect_to manage_users_path
+    end
 end
