d14ef7e82ece4fae9ee7b4d93ddd856c89025be7
Rich Lobo <lobo.r0018@gmail.com>
2024-08-03 21:49:55 +0800

    Use uuid for primary key

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_03_091536) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_03_134738) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
