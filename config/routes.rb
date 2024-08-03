11b60ba3aadee60ed5c1d0ecaeec7c37649400c3
Rich Lobo <lobo.r0018@gmail.com>
2024-08-03 16:29:38 +0800

    WIP manage users

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
     get "sign_in" => "users/sessions#new" 
   end
   get 'home/index'
+  resources :manage_users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
