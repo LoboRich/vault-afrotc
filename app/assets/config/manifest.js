c630691c18ecc7843932b5210c0ca79c8728365e
Keren Rogel <kerenrogel@gmail.com>
2024-08-20 20:00:13 +0800

    Revert

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,7 +1,5 @@
 //= link_tree ../images
-//= link controllers/counter_controller.js
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
-//= link application.scss
 //= link_tree ../builds
