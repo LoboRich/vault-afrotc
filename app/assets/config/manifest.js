363f0b761bea540bfc1fcdd8187938c8da729a81
Keren Rogel <kerenrogel@gmail.com>
2024-08-21 13:13:23 +0800

    Updates files for Bootstrap

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,8 @@
 //= link_tree ../images
-//= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
 //= link_tree ../builds
+//= link custom.css
+//= link main.css
+//= link bootstrap.css
+//= link pace.css
