ea8f4510ac0af3a60d8bd30a5de3b47ce491d765
Rich Lobo <lobo.r0018@gmail.com>
2024-08-26 19:59:25 +0800

    WIP loan

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -64,7 +64,7 @@ class LoansController < ApplicationController
     end
 
     # Only allow a list of trusted parameters through.
-    def loan_params
-      params.require(:loan).permit(:client_id, :blocklot, :terms, :model_house, :loan_financing, :contract_price, :processing_fees, :downpayment, :interest, :principal, :monthly_amort, :contract_date, :amortization_start_date, :balance, :remarks, :status, :broker)
+    def loan_params 
+      params.require(:loan).permit(:client_id, {:blocklot => []}, :terms, :model_house, :loan_financing, :contract_price, :processing_fees, :downpayment, :interest, :principal, :monthly_amort, :contract_date, :amortization_start_date, :balance, :remarks, :status, :broker)
     end
 end
