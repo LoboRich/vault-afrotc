73823596e177d19a33ce81eea09f1b28174cd6b8
Rich Lobo <lobo.r0018@gmail.com>
2024-08-26 10:29:25 +0800

    Adds subdivisions table

---
+++ app/views/subdivisions/_subdivision.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! subdivision, :id, :name, :short_code, :location, :status, :created_at, :updated_at
+json.url subdivision_url(subdivision, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_26_012949) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_26_022804) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -50,6 +50,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_26_012949) do
   create_table "parcels", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "block"
     t.string "lot"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "subdivisions", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "name"
+    t.string "short_code"
+    t.string "location"
     t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
