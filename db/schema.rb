527275583d153d40cd2b42604f7656727d2d25ef
Rich Lobo <lobo.r0018@gmail.com>
2024-09-22 22:53:52 +0800

    Adds addtl fields on hostoiry table

--- db/schema.rb
+++ db/schema.rb
@@ -50,6 +50,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_22_133843) do
   create_table "histories", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.bigint "user_id", null: false
     t.text "description"
+    t.string "model"
+    t.string "model_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_histories_on_user_id"
