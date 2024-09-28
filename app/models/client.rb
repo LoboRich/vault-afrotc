196dbe9c05296668c5dbb084728ce9a19ab147b3
Rich Lobo <lobo.r0018@gmail.com>
2024-09-28 09:27:08 +0800

    Multiple upload files in client

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,10 +1,10 @@
 class Client < ApplicationRecord
     mount_uploaders :birth_cert_img, ImageUploader
-	# mount_uploader :valid_id_img, ImageUploader
-    # mount_uploader :proof_of_income_img, ImageUploader
-	# mount_uploader :marriage_contract_img, ImageUploader
-    # mount_uploader :spa_img, ImageUploader
-	# mount_uploader :pdc_img, ImageUploader
+	mount_uploaders :valid_id_img, ImageUploader
+    mount_uploaders :proof_of_income_img, ImageUploader
+	mount_uploaders :marriage_contract_img, ImageUploader
+    mount_uploaders :spa_img, ImageUploader
+	mount_uploaders :pdc_img, ImageUploader
     # mount_uploader :doc, ImageUploader
 
     has_many :loans
