c1e8a14916969a908a2ba106041f676bace46ed9
Rich Lobo <lobo.r0018@gmail.com>
2024-09-29 21:26:43 +0800

    Dashboard

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
 class HomeController < ApplicationController
   def index
+    @this_month_collectibles = LoanItem.where('duedate BETWEEN ? AND ?', Date.today.beginning_of_month, Date.today.end_of_month)
+    @total_collectibles = @this_month_collectibles.sum(:monthly_amort)
+    @collected = @this_month_collectibles.where(is_paid: true).sum(:paid_amount)
+
+    @this_month_water_collectibles = WaterBill.where('due_date BETWEEN ? AND ?', Date.today.beginning_of_month, Date.today.end_of_month)
+    @total_water_collectibles = @this_month_water_collectibles.sum(:amount)
+    @water_collected = @this_month_water_collectibles.where(status: 'paid').sum(:amount)
   end
 end
