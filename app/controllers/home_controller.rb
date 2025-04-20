224875eb4014b60f549218a9d8d85fae95eb22ca
Rich Lobo <lobo.r0018@gmail.com>
2025-04-20 13:36:27 +0800

    Cleanup and new db

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,12 @@
+class HomeController < ApplicationController
+  def index
+    @total_clients = 0
+    @total_inventories = 0
+    @this_month_collectibles = []
+    @total_collectibles = 0
+    @collected = 0
+    @this_month_water_collectibles = []
+    @total_water_collectibles = 0
+    @water_collected = 0
+  end
+end
