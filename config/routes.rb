d99bb8cd0cd9e5b27da0fb966b7ff91cb063efe2
Rich Lobo <lobo.r0018@gmail.com>
2025-07-23 17:38:30 +0800

    Scaffolds Attachment

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :attachments
   resources :vehicles
   resources :franchises
   resources :authorize_routes
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_07_23_055813) do
+ActiveRecord::Schema[7.1].define(version: 2025_07_23_093753) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
+
+  create_table "attachments", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.uuid "vehicle_id"
+    t.uuid "franchise_id"
+    t.string "image"
+    t.string "filename"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "authorize_routes", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "name"
