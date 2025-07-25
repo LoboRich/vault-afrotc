a60b3f38d90af91ef36f1fe6a3b344e0a3750437
Rich Lobo <lobo.r0018@gmail.com>
2025-07-25 22:01:04 +0800

    Whitelist is_registered and update method for updating vehicle

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -39,14 +39,13 @@ class VehiclesController < ApplicationController
 
   # PATCH/PUT /vehicles/1 or /vehicles/1.json
   def update
+    @vehicle.update!(vehicle_params.except(:attachments))
+    delete_photos
+    store_photos
+
     respond_to do |format|
-      if @vehicle.update(vehicle_params)
-        format.html { redirect_to @vehicle, notice: "Vehicle was successfully updated." }
-        format.json { render :show, status: :ok, location: @vehicle }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @vehicle.errors, status: :unprocessable_entity }
-      end
+      format.html { redirect_to @vehicle, notice: 'Vehicle was successfully updated.' }
+      format.json { render :show }
     end
   end
 
@@ -68,7 +67,7 @@ class VehiclesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def vehicle_params
-      params.require(:vehicle).permit(:classification, :registered_owner, :bus_num, :mv_file, :plate_num, :ending_num, :body_type, :denomination, :motor_num, :chassis_num, :gross_weight, :net_capacity, :seat_capacity, :make, :year_model, :fuel, :num_of_cyclinder, :field_office, :cr_num, :cr_date_issue, :or_field_office, :or_num, :or_date_issue, :reg_amount, :body_color, :coc_date_coverage, :coc_num, :coc_amount, :insurance_name, :status, :book_value, :date_operate, :date_retired, :remarks, :coi_date_coverage, :quality_type, :coi_num, :coi_amount, :piston_displacement, :category, :bus_series, :present_holder, :penalties_surcharge, :cr_field_office, :type_of_vehicle, :route_per_operation, :year_rebuild, :authorize_route_id, :franchise_id, {:attachments => []})
+      params.require(:vehicle).permit(:classification, :registered_owner, :bus_num, :mv_file, :plate_num, :ending_num, :body_type, :denomination, :motor_num, :chassis_num, :gross_weight, :net_capacity, :seat_capacity, :make, :year_model, :fuel, :num_of_cyclinder, :field_office, :cr_num, :cr_date_issue, :or_field_office, :or_num, :or_date_issue, :reg_amount, :body_color, :coc_date_coverage, :coc_num, :coc_amount, :insurance_name, :status, :book_value, :date_operate, :date_retired, :remarks, :coi_date_coverage, :quality_type, :coi_num, :coi_amount, :piston_displacement, :category, :bus_series, :present_holder, :penalties_surcharge, :cr_field_office, :type_of_vehicle, :route_per_operation, :year_rebuild, :authorize_route_id, :franchise_id, {:attachments => []}, :is_registered)
     end
 
     def store_photos
