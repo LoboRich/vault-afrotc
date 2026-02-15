6b4a1295a3442a12e6e88545e46ecadd3cee0725
Rich Lobo <lobo.r0018@gmail.com>
2026-02-16 00:09:35 +0800

    Fixes

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -3,9 +3,6 @@
 //= link_tree ../../../vendor/javascript .js
 //= link_tree ../images
 //= link_directory ../stylesheets .css
-//= link main.css
-//= link main.min.css
-//= link pace.css
 //= link bootstrap.min.js
 //= link jquery-3.5.1.min.js
 //= link main.min.js
