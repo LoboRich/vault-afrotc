6868fe5a3ff244a364f37a4d086e374bafcaa8d5
Rich Lobo <lobo.r0018@gmail.com>
2026-02-18 14:45:00 +0800

    Fix manifest error

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -5,7 +5,6 @@
 //= link_directory ../stylesheets .css
 //= link bootstrap.min.js
 //= link jquery-3.5.1.min.js
-//= link main.min.js
 //= link datatables.js
 //= link rails-ujs.js
 //= link logo.png
