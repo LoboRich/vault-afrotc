9ceb794f194fadf5b096aca98e391f73ab58a656
Rich Lobo <lobo.r0018@gmail.com>
2026-02-18 22:53:02 +0800

    WIP dashboard ui and data

--- Gemfile
+++ Gemfile
@@ -62,6 +62,7 @@ gem "sassc-rails"
 gem "geocoder"
 gem 'wicked_pdf'
 gem 'wkhtmltopdf-binary'
+gem 'groupdate'
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+    @total_reservists = Reservist.count
+    @active_reservists = Reservist.where(is_active: true).count
+    @inactive_reservists = Reservist.where(is_active: false).count
+
+    @monthly_data = Reservist.group_by_month(:created_at, last: 6) .count
   end
 end
