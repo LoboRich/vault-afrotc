977dee41d337cb3d3caab50b3eaf0fab85806693
Rich Lobo <lobo.r0018@gmail.com>
2026-02-19 14:35:40 +0800

    Finishes dashboard

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,14 @@
 class HomeController < ApplicationController
   def index
-    @total_reservists = Reservist.count
-    @active_reservists = Reservist.where(is_active: true).count
-    @inactive_reservists = Reservist.where(is_active: false).count
-    @monthly_data = Reservist.group_by_month(:created_at, last: 6) .count
-
+    @reservists = Reservist.all
+    @total_reservists = @reservists.count
+    @active_count   = @reservists.where(is_active: true).count
+    @inactive_count = @reservists.where(is_active: false).count + @reservists.where(is_active: nil).count
     # linegraph data for events
-    @years = Event.pluck(Arel.sql("DISTINCT EXTRACT(YEAR FROM event_date)"))
+    @events = Event.all
+    @total_events = @events.count
+    @total_trainings = @events.where(event_type: "training").count
+    @years = @events.pluck(Arel.sql("DISTINCT EXTRACT(YEAR FROM event_date)"))
                 .map(&:to_i)
                 .sort
                 .reverse
@@ -16,5 +18,23 @@ class HomeController < ApplicationController
                     .where("EXTRACT(YEAR FROM event_date) = ?", @selected_year)
                     .group_by_month(:event_date)
                     .count
+
+    # bargraph data for announcements
+    @years = Announcemnt.pluck(Arel.sql("DISTINCT EXTRACT(YEAR FROM created_at)")).map(&:to_i).sort.reverse
+
+    @selected_year  = params[:year].presence || Date.current.year
+    @selected_month = params[:month]
+
+    announcements = Announcemnt.all
+
+    if @selected_year.present?
+      announcements = announcements.where("EXTRACT(YEAR FROM created_at) = ?", @selected_year.to_i)
+    end
+
+    if @selected_month.present?
+      announcements = announcements.where("EXTRACT(MONTH FROM created_at) = ?", @selected_month.to_i)
+    end
+
+    @announcement_data = announcements.group_by_month(:created_at, format: "%b").count
   end
 end
