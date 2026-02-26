9d9a068346cd2912e83d639da54cbfb6b2cbebfa
Rich Lobo <lobo.r0018@gmail.com>
2026-02-26 14:57:36 +0800

    Fix UI and limit reservist adt application

--- app/models/adt.rb
+++ app/models/adt.rb
@@ -1,3 +1,15 @@
 class Adt < ApplicationRecord
   belongs_to :reservist
+  validate :six_months_between_applications, on: :create
+  
+  private
+
+  def six_months_between_applications
+    last_adt = reservist.adts.order(created_at: :desc).first
+    return unless last_adt
+
+    if last_adt.created_at > 6.months.ago
+      errors.add(:base, "You can only submit a new application 6 months after your last one.")
+    end
+  end
 end
--- app/models/reservist.rb
+++ app/models/reservist.rb
@@ -1,5 +1,6 @@
 class Reservist < ApplicationRecord
     belongs_to :user, optional: true
+    has_many :adts, class_name: "Adt", foreign_key: "reservist_id", dependent: :destroy
 
     def full_name
       "#{last_name}, #{first_name} #{middle_name}"
