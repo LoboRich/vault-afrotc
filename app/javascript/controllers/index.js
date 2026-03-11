623dfb59a944546af9e04b0ff375fdea0d93ebfd
Rich Lobo <lobo.r0018@gmail.com>
2026-03-11 16:32:39 +0800

    Fix Stimulus controllers and importmap for Heroku

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,14 +2,13 @@
 // Run that command whenever you add a new controller or create them with
 // ./bin/rails generate stimulus controllerName
 
-import { application } from "controllers/application"
+import { application } from "./application"
 
 import HelloController from "./hello_controller"
+application.register("hello", HelloController)
 
 import LocationController from "./location_controller"
+application.register("location", LocationController)
 
 import ReservistLocationController from "./reservist_location_controller"
-
-application.register("hello", HelloController)
-application.register("location", LocationController)
 application.register("reservist-location", ReservistLocationController)
