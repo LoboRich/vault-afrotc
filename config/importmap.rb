81a01973ed01aabb75022f866a7eeece7130155a
Rich Lobo <lobo.r0018@gmail.com>
2026-03-11 14:52:50 +0800

    Fix broken importmap pins

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,21 +1,19 @@
-# Pin npm packages by running ./bin/importmap
-
 pin "application"
-pin "@hotwired/turbo-rails", to: "@hotwired--turbo-rails.js" # @8.0.16
-pin "@hotwired/turbo", to: "@hotwired--turbo.js" # @8.0.13
+pin "@hotwired/turbo-rails", to: "@hotwired--turbo-rails.js"
+pin "@hotwired/turbo", to: "@hotwired--turbo.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "@rails/ujs", to: "@rails--ujs.js" # @7.1.3
-pin "chartkick" # @5.0.1
-pin "Chart.bundle", to: "Chart.bundle.js"
-pin "@rails/actioncable/src", to: "@rails--actioncable--src.js" # @8.0.200
 
+pin "@rails/ujs", to: "@rails--ujs.js"
 
-pin "chart.js" # @4.5.0
-pin "@kurkle/color", to: "@kurkle--color.js" # @0.3.4
+pin "chart.js"
 pin "chartkick", to: "chartkick.js"
-pin "Chart.bundle", to: "Chart.bundle.js" # optional for Chart.js integrationpin "jquery" # @4.0.0
-pin "datatables.net" # @2.3.7
-pin "datatables.net-bs5" # @2.3.7
-pin "simple-datatables" # @10.2.0
+pin "@kurkle/color", to: "@kurkle--color.js"
+
+pin "simple-datatables"
+pin "datatables.net"
+pin "datatables.net-bs5"
+
+pin "@rails/actioncable/src", to: "@rails--actioncable--src.js"
