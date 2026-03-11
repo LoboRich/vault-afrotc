01cb3c7b6af49f41f267d752dea0f29f8b111427
Rich Lobo <lobo.r0018@gmail.com>
2026-03-11 16:08:50 +0800

    Fix Stimulus js

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,10 +1,9 @@
 import { Application } from "@hotwired/stimulus"
-import { definitionsFromContext } from "@hotwired/stimulus-loading"
 
 const application = Application.start()
 
-// Automatically load all controllers from this directory
-const context = require.context(".", true, /\.js$/)
-application.load(definitionsFromContext(context))
+// Configure Stimulus development experience
+application.debug = false
+window.Stimulus   = application
 
 export { application }
