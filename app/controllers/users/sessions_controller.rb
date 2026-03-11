0ba874a16f75dd2bd78e7b380655d2bb0d504853
Rich Lobo <lobo.r0018@gmail.com>
2026-03-11 13:30:22 +0800

    Addds missing link to sidebar and change sms number

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -6,7 +6,7 @@ class Users::SessionsController < Devise::SessionsController
     OtpMailer.new_otp(resource).deliver_now
 
     api = SmsApi.new
-    if api.send_message(message: "Hi! Your OTP code is: #{resource.otp_code}", number: "09164745123")
+    if api.send_message(message: "Hi! Your OTP code is: #{resource.otp_code}", number: "09276112312")
       print 'Message sent!'
     else
       print 'Message sending failed!'
