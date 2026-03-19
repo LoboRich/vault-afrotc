9a82e92cf95ac22c023c6a952080b6efde4a1813
Rich Lobo <lobo.r0018@gmail.com>
2026-03-19 17:21:10 +0800

    History

--- app/controllers/history_controller.rb
+++ app/controllers/history_controller.rb
@@ -2,7 +2,10 @@ class HistoryController < ApplicationController
   def index
     @histories = [
       OpenStruct.new(
-        user: "admin@gmail.com",
+        user: {
+          fullname: "John Voegeler",
+          user_id: "user_123"
+        },
         action: "update",
         record_type: "Reservist",
         record_id: "https://afrotc-vault-58e61a936423.herokuapp.com/reservists/8d52eacc-1ce1-49f6-b8e8-6b71b43ae139",
@@ -13,7 +16,10 @@ class HistoryController < ApplicationController
         timestamp: Time.now
       ),
       OpenStruct.new(
-        user: "admin@gmail.com",
+        user: {
+          fullname: "Louie De Palma",
+          user_id: "user_123"
+        },
         action: "update",
         record_type: "Reservist",
         record_id: "https://afrotc-vault-58e61a936423.herokuapp.com/reservists/8d52eacc-1ce1-49f6-b8e8-6b71b43ae139",
@@ -21,7 +27,10 @@ class HistoryController < ApplicationController
         timestamp: Time.now
       ),
       OpenStruct.new(
-        user: "admin@gmail.com",
+        user: {
+          fullname: "Louie De Palma",
+          user_id: "user_123"
+        },
         action: "create",
         record_type: "Emergency Notification",
         record_id: "https://afrotc-vault-58e61a936423.herokuapp.com/announcemnts/7b1533f4-67c1-4dbb-a968-2eac4efe461a",
@@ -29,11 +38,75 @@ class HistoryController < ApplicationController
         timestamp: Time.now - 3.hours
       ),
       OpenStruct.new(
-        user: "admin@gmail.com",
+        user: {
+          fullname: "John Voegeler",
+          user_id: "user_123"
+        },
         action: "download",
         record_type: "ADT",
         record_id: "https://afrotc-vault-58e61a936423.herokuapp.com/announcemnts/7b1533f4-67c1-4dbb-a968-2eac4efe461a",
-        changes: "Downloaded ADT Report: Training Schedule",
+        changes: "Downloaded ADT Application Form",
+        timestamp: Time.now - 3.hours
+      ),
+      OpenStruct.new(
+        user: {
+          fullname: "Louie De Palma",
+          user_id: "user_123"
+        },
+        action: "create",
+        record_type: "Emergency Notification",
+        record_id: "https://afrotc-vault-58e61a936423.herokuapp.com/announcemnts/7b1533f4-67c1-4dbb-a968-2eac4efe461a",
+        changes: "Created Emergency Notification: Training Schedule",
+        timestamp: Time.now - 16.hours
+      ),
+      OpenStruct.new(
+        user: {
+          fullname: "Louie De Palma",
+          user_id: "user_123"
+        },
+        action: "create",
+        record_id: "https://afrotc-vault-58e61a936423.herokuapp.com/events/736a3836-1efd-4d99-aed1-d1f382c7bcbe",
+        changes: "Created Event: Basic Weapon Handling",
+        timestamp: Time.now - 16.hours
+      ),
+      OpenStruct.new(
+        user: {
+          fullname: "Christian Vaughn",
+          user_id: "user_178732"
+        },
+        action: "update",
+        record_id: "https://afrotc-vault-58e61a936423.herokuapp.com/events/736a3836-1efd-4d99-aed1-d1f382c7bcbe",
+        changes: "Uploaded Profile Image",
+        timestamp: Time.now - 16.hours
+      ),
+      OpenStruct.new(
+        user: {
+          fullname: "Kathryn Janeway",
+          user_id: "user_178732"
+        },
+        action: "update",
+        record_id: "https://afrotc-vault-58e61a936423.herokuapp.com/events/736a3836-1efd-4d99-aed1-d1f382c7bcbe",
+        changes: "Updated Account Information",
+        timestamp: Time.now - 16.hours
+      ),
+      OpenStruct.new(
+        user: {
+          fullname: "Jovi Lopez",
+          user_id: "user_178732"
+        },
+        action: "update",
+        record_id: "https://afrotc-vault-58e61a936423.herokuapp.com/events/736a3836-1efd-4d99-aed1-d1f382c7bcbe",
+        changes: "Updated Account Information",
+        timestamp: Time.now - 16.hours
+      ),
+      OpenStruct.new(
+        user: {
+          fullname: "Lucy Heartfilia",
+          user_id: "user_178732"
+        },
+        action: "create",
+        record_id: "https://afrotc-vault-58e61a936423.herokuapp.com/events/736a3836-1efd-4d99-aed1-d1f382c7bcbe",
+        changes: "Created new ADT Application",
         timestamp: Time.now - 3.hours
       )
     ]
